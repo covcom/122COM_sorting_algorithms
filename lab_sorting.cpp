467f4912f3b48b7854dab545edfe215b1e7ee095
dscroft <dscroft@gmail.com>
2016-04-13 10:09:01 +0100

    Added solutions.zip

--- lab_sorting.cpp
+++ lab_sorting.cpp
@@ -9,41 +9,41 @@ using namespace std;
 
 vector<int> bubble_sort( vector<int> sequence )
 {
-	// COMPLETE ME
+    // COMPLETE ME
 
-	return sequence;
+    return sequence;
 }
 
 vector<int> selection_sort( vector<int> sequence )
 {
-	// COMPLETE ME
+    // COMPLETE ME
 
-	return sequence;
+    return sequence;
 }
 
 vector<int> quick_sort( vector<int> sequence )
 {
-	// COMPLETE ME
+    // COMPLETE ME
 
-	return sequence;
+    return sequence;
 }
 
 void _quick_sort_inplace( vector<int>& sequence, int start, int end )
 {
-	// COMPLETE ME
+    // COMPLETE ME
 }
 
 vector<int> quick_sort_inplace( vector<int> sequence )
 {
-	_quick_sort_inplace( sequence, 0, sequence.size()-1 );
-	return sequence;
+    _quick_sort_inplace( sequence, 0, sequence.size()-1 );
+    return sequence;
 }
 
 vector<int> merge_sort( vector<int> sequence )
 {
-	// COMPLETE ME
+    // COMPLETE ME
 
-	return sequence;
+    return sequence;
 }
 
 
@@ -54,107 +54,112 @@ vector<int> merge_sort( vector<int> sequence )
 template<typename T>
 string vec_to_str( vector<T>& sequence )
 {
-	stringstream ss;
-	for( T i : sequence )
-		ss << i << ", ";
-	
-	return ss.str();
+    stringstream ss;
+    for( T i : sequence )
+        ss << i << ", ";
+    
+    return ss.str();
 }
 
 int main()
 {
-	array< pair< string, vector<int>(*)(vector<int>) >, 5 > sortingAlgorithms;
-	sortingAlgorithms[0] = make_pair("bubblesort", &bubble_sort );
-	sortingAlgorithms[1] = make_pair("selection sort", &selection_sort );
-	sortingAlgorithms[2] = make_pair("quicksort", &quick_sort );
-	sortingAlgorithms[3] = make_pair("quick inplace", &quick_sort_inplace );
-	sortingAlgorithms[4] = make_pair("merge", &merge_sort );
+    vector< pair< string, vector<int>(*)(vector<int>) > > sortingAlgorithms { 
+            make_pair("bubblesort", &bubble_sort ),
+            make_pair("selection sort", &selection_sort ),
+            make_pair("quicksort", &quick_sort ),
+            make_pair("quick inplace", &quick_sort_inplace ),
+            make_pair("merge", &merge_sort ) };
 
-	set<string> working;
-	for( auto i : sortingAlgorithms ) { working.insert( i.first ); }
+    set<string> working;
+    for( auto i : sortingAlgorithms ) { working.insert( i.first ); }
 
-	/* ==============================================
-	 	This is the small collection of numbers test
-	   ============================================== */
-	// generate 10 random numbers between -100 and 100 so we can see that it's working
-	vector<int> smallNumbers, smallCorrect;
-	smallNumbers.resize(10);
-	for( int& i : smallNumbers )
-	{
-		i = rand() % 200 -100;
-	}
-	
-	// print out the small numbers
-	cout << "SMALL SEQUENCE TEST" << endl;
-	cout << setw(16) << "starting numbers: " << vec_to_str( smallNumbers ) << endl;
+    /* ==============================================
+         This is the small collection of numbers test
+       ============================================== */
+    // generate 10 random numbers between -100 and 100 so we can see that it's working
+    vector<int> smallNumbers, smallCorrect;
+    smallNumbers.resize(10);
+    for( int& i : smallNumbers )
+    {
+        i = rand() % 200 -100;
+    }
+    
+    // print out the small numbers
+    cout << "SMALL SEQUENCE TEST" << endl;
+    cout << setw(16) << "starting numbers: " << vec_to_str( smallNumbers ) << endl;
 
-	// sort the small numbers
-	smallCorrect = smallNumbers;
-	sort( smallCorrect.begin(), smallCorrect.end() );
-	cout << setw(16) << "correctly sorted: " << vec_to_str( smallCorrect ) << endl;	
+    // sort the small numbers
+    smallCorrect = smallNumbers;
+    sort( smallCorrect.begin(), smallCorrect.end() );
+    cout << setw(16) << "correctly sorted: " << vec_to_str( smallCorrect ) << endl;    
 
-	for( auto i : sortingAlgorithms )
-	{
-		vector<int>result = i.second( smallNumbers );
+    for( auto i : sortingAlgorithms )
+    {
+        vector<int>result = i.second( smallNumbers );
 
-		cout << setw(16) << i.first << ": " << vec_to_str( result ) << endl;
+        cout << setw(16) << i.first << ": " << vec_to_str( result ) << endl;
 
-		if( result != smallCorrect )
-		{
-			working.erase( i.first );
-		}
-	}
+        /* we're going to remove the names of the ones that don't work rather than 
+            add the ones that do work in case it fails the first test but passes the
+            second */
+        if( result != smallCorrect )
+        {
+            working.erase( i.first );    
+        }
+    }
 
-	for( auto i : sortingAlgorithms )
-	{
-		if( working.find(i.first) != working.end() )
-		{
-			cout << i.first << " worked" << endl;
-		}
-		else
-		{
-			cout << i.first << " failed" << endl;
-		}
-	}
+    for( auto i : sortingAlgorithms )
+    {
+        if( working.find(i.first) != working.end() )
+        {
+            cout << i.first << " worked" << endl;
+        }
+        else
+        {
+            cout << i.first << " failed" << endl;
+        }
+    }
 
-	cout << endl;
+    cout << endl;
 
-	/* ==============================================
-		This is the big collection of numbers test
-	   ============================================== */
-	// generate 5000 random numbers so we can profile our code
-	vector<int> bigNumbers, bigCorrect;
-	bigNumbers.resize(10000);
-	for( int& i : bigNumbers )
-	{
-		i = rand();
-	}
+    /* ==============================================
+        This is the big collection of numbers test
+       ============================================== */
+    // generate 5000 random numbers so we can profile our code
+    vector<int> bigNumbers, bigCorrect;
+    bigNumbers.resize(10000);
+    for( int& i : bigNumbers )
+    {
+        i = rand();
+    }
 
-	cout << "BIG SEQUENCE TEST" << endl;
+    cout << "BIG SEQUENCE TEST" << endl;
 
-	// sort the big numbers
-	bigCorrect = bigNumbers;
-	sort( bigCorrect.begin(), bigCorrect.end() );
+    // sort the big numbers
+    bigCorrect = bigNumbers;
+    sort( bigCorrect.begin(), bigCorrect.end() );
 
-	for( auto i : sortingAlgorithms )
-	{
-		vector<int> result = i.second( bigNumbers );
+    for( auto i : sortingAlgorithms )
+    {
+        vector<int> result = i.second( bigNumbers );
 
-		if( result != bigCorrect )
-		{
-			working.erase( i.first );
-		}
-	}
+        if( result != bigCorrect )
+        {
+            working.erase( i.first );
+        }
+    }
 
-	for( auto i : sortingAlgorithms )
-	{
-		if( working.find(i.first) != working.end() )
-		{
-			cout << i.first << " worked" << endl;
-		}
-		else
-		{
-			cout << i.first << " failed" << endl;
-		}
-	}
+    for( auto i : sortingAlgorithms )
+    {
+        if( working.find(i.first) != working.end() )
+        {
+            cout << i.first << " worked" << endl;
+        }
+        else
+        {
+            cout << i.first << " failed" << endl;
+        }
+    }
+
+    return sortingAlgorithms.size() != working.size();
 }
--- lab_sorting.py
+++ lab_sorting.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 
 def bubble_sort( sequence ):
-	# COMPLETE ME - Green task
+    # COMPLETE ME - Green task
 
-	return sequence
+    return sequence
 
 def selection_sort( sequence ):
-	# COMPLETE ME - Yellow task
+    # COMPLETE ME - Yellow task
 
-	return sequence
+    return sequence
 
 def quick_sort( sequence ):
-	# COMPLETE ME - Yellow task
+    # COMPLETE ME - Yellow task
 
-	return sequence
+    return sequence
 
-def quick_sort_inplace( sequence ):
-	# COMPLETE ME - Red task
+def quick_sort_inplace( sequence, start=None, end=None ):
+    # COMPLETE ME - Red task
 
-	return sequence
+    return sequence
 
 def merge_sort( sequence ):
-	# COMPLETE ME - Red task
+    # COMPLETE ME - Red task
 
-	return sequence
+    return sequence
 
 
 
@@ -35,65 +35,68 @@ def merge_sort( sequence ):
 
 
 if __name__ == '__main__':
-	import random, copy
+    import random, copy, sys
 
-	sortingAlgorithms = {'bubblesort': bubble_sort,
-						'selection sort': selection_sort,
-						'quicksort': quick_sort,
-						'quick inplace': quick_sort_inplace,
-						'merge sort': merge_sort}
-	worked = set(sortingAlgorithms.keys())
+    sortingAlgorithms = {'bubblesort': bubble_sort,
+                        'selection sort': selection_sort,
+                        'quicksort': quick_sort,
+                        'quick inplace': quick_sort_inplace,
+                        'merge sort': merge_sort}
+    worked = set(sortingAlgorithms.keys())
 
-	# ==============================================
-	#	This is the small collection of numbers test
-	# ============================================== 
-	# generate 10 random numbers between -100 and 100 so we can see that it's working
-	smallNumbers = [ random.randint(-100,100) for i in range(10) ]
-	
-	# print out the small numbers
-	print( 'SMALL SEQUENCE TEST' )
-	print( 'starting numbers:', smallNumbers )
+    # ==============================================
+    #    This is the small collection of numbers test
+    # ============================================== 
+    # generate 10 random numbers between -100 and 100 so we can see that it's working
+    smallNumbers = [ random.randint(-100,100) for i in range(10) ]
+    
+    # print out the small numbers
+    print( 'SMALL SEQUENCE TEST' )
+    print( 'starting numbers:', smallNumbers )
 
-	# sort the small numbers
-	smallCorrect = sorted( smallNumbers )
-	print( 'correctly sorted:', smallCorrect )
+    # sort the small numbers
+    smallCorrect = sorted( smallNumbers )
+    print( 'correctly sorted:', smallCorrect )
 
-	for name, function in sortingAlgorithms.items():
-		result = function( copy.copy(smallNumbers) )
-		print( '%s:' % name.rjust(16), result )
+    for name, function in sortingAlgorithms.items():
+        result = function( copy.copy(smallNumbers) )
+        print( '%s:' % name.rjust(16), result )
 
-		if result != smallCorrect:
-			worked.discard( name )
+        # we're going to remove the names of the ones that don't work rather than 
+        #    add the ones that do work in case it fails the first test but passes the
+        #    second 
+        if result != smallCorrect:
+            worked.discard( name )
 
-	for name in sortingAlgorithms.keys():
-		if name in worked:
-			print( '%s worked' % name )
-		else:
-			print( '%s failed' % name )
+    for name in sortingAlgorithms.keys():
+        if name in worked:
+            print( '%s worked' % name )
+        else:
+            print( '%s failed' % name )
 
-	print()
+    print()
 
-	# ==============================================
-	#	This is the big collection of numbers test
-	# ============================================== 
-	# generate 5000 random numbers so we can profile our code
-	bigNumbers = [ random.random() for i in range(5000) ]
+    # ==============================================
+    #    This is the big collection of numbers test
+    # ============================================== 
+    # generate 5000 random numbers so we can profile our code
+    bigNumbers = [ random.random() for i in range(5000) ]
 
-	print( 'BIG SEQUENCE TEST' )
+    print( 'BIG SEQUENCE TEST' )
 
-	# sort the big numbers
-	bigCorrect = sorted( bigNumbers )
+    # sort the big numbers
+    bigCorrect = sorted( bigNumbers )
 
-	for name, function in sortingAlgorithms.items():
-		result = function(bigNumbers)
+    for name, function in sortingAlgorithms.items():
+        result = function(bigNumbers)
 
-		if result != bigCorrect:
-			worked.discard( name )
+        if result != bigCorrect:
+            worked.discard( name )
 
-	for name in sortingAlgorithms.keys():
-		if name in worked:
-			print( '%s passed' % name )
-		else:
-			print( '%s failed' % name )	
+    for name in sortingAlgorithms.keys():
+        if name in worked:
+            print( '%s passed' % name )
+        else:
+            print( '%s failed' % name )    
 
-
+    sys.exit( len(worked) != len(sortingAlgorithms) )
